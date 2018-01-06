64e3f8bad2a322d2232655044dc8c57b80c99d26
Yassir <hartaniyassir@gmail.com>
2018-01-06 15:19:10 +0000

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params) 
     
     if @user.save
+      login @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user #this line redirect to the current  /users/id that has been created
     else
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,18 @@ class User < ApplicationRecord
                    
   has_secure_password
   
+  # Returns the hash digest of the given string.
+  def User.digest(string)
+    # what i understand is that we need a secure password that 
+    # have a low cost (parameter that determines the computational cost to calculate the hash )
+    # which means a minimun time 
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                              BCrypt::Engine.cost
+                                              
+    BCrypt::Password.create(string, cost: cost)
+                                          
+  end
+  
   validates :password , presence: true , length: {minimum: 6 }
 end
 
